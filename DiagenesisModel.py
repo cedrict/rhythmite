1f15c7b92fb41f01e8b4df8c50b3bfb7840cbde6
cedric thieulot <c.thieulot@uu.nl>
2024-05-16 22:20:57 +0200

    adding manual

--- DiagenesisModel.py
+++ DiagenesisModel.py
@@ -25,7 +25,6 @@ def heaviside(x,xbot,xtop,xscale):
     val=0.5*(1+np.tanh((x-xtop/xscale)*500)) *0.5*(1+np.tanh((xbot/xscale-x)*500))
     return val
 
-
 ###############################################################################
 
 # for the jit-compiling, we need to specify the type of all parameters in the class
@@ -117,8 +116,7 @@ class LHeureux:
         self.c_co_0 = 0.326
         self.phi_0 = 0.8            # steady state 0.6, oscillations 0.8
 
-        self.rho_s0 = 2.95*self.AR_0 + 2.71*self.CA_0 + 2.8*(1 - (self.AR_0 + self.CA_0)) # initial sediment density (g/cm^3)
-
+        self.rho_s0 = 2.95*self.AR_0+2.71*self.CA_0+2.8*(1-(self.AR_0+self.CA_0)) # initial sediment density (g/cm^3)
 
         # initial condtions
         self.AR_init = self.AR_0
@@ -129,8 +127,9 @@ class LHeureux:
 
         # more params (which depend on initial/boundary conditions)
         self.delta = self.rho_s0/(self.muA*np.sqrt(self.K_C))   # part of the Ca, CO3 reaction terms (cm^-3)
-        self.phi_NR = self.phi_init                                  # porosity in the absence of reactions
-        self.phi_inf = 0.01                                          # "a parameter" in Eqn 23
+        self.phi_NR = self.phi_init                             # porosity in the absence of reactions
+        self.phi_inf = 0.01                                     # "a parameter" in Eqn 23
+
         # porosity diffusion coefficient
         self.d_phi = self.beta*(self.phi_init**3 / ( 1 - self.phi_init ) )*\
                     (1 / ( self.b*self.g*self.rho_w*( self.phi_NR - self.phi_inf ) ) )*\
@@ -142,31 +141,36 @@ class LHeureux:
         self.v_scale = self.sed_rate
         
         self.Da = self.k2*self.t_scale    # Damkohler number
-        
-        
-    ##### functions for calculating RHS's of eqns 40-43  #####
     
+    ################################################
     # hydraulic conductivity
+    ################################################
+
     def K(self,phi):
-        return self.beta*( phi**3 / ( 1 - phi )**2 )*( 1 - np.exp(-10*( 1 - phi) / phi ) )
+        return self.beta*(phi**3/(1-phi)**2)*(1-np.exp(-10*(1-phi)/phi))
     
+    ################################################
     # velocities
+    ################################################
+
     def U(self, phi):
         # solid velocity, eqn 46
-        u = 1 - ( 1 / self.sed_rate )*\
-            ( self.K(self.phi_0) * ( 1 - self.phi_0 ) - self.K(phi) * ( 1 - phi ) )*\
-            ( self.rho_s0 / self.rho_w - 1 )
+        u = 1-(1/self.sed_rate)*\
+            (self.K(self.phi_0) * ( 1 - self.phi_0 ) - self.K(phi)*(1-phi))*\
+            (self.rho_s0 / self.rho_w - 1 )
         return u
 
     def W(self,phi):
         # solute velocity, eqn 47
-        w = 1 - ( 1 / self.sed_rate )*\
-            ( self.K(self.phi_0) * ( 1 - self.phi_0 ) + self.K(phi) * ( 1 - phi )**2 / phi )*\
-            ( self.rho_s0 / self.rho_w - 1 )
-        
+        w = 1-(1/self.sed_rate)*\
+            (self.K(self.phi_0) * ( 1 - self.phi_0 ) + self.K(phi)*(1-phi)**2/phi)*\
+            (self.rho_s0 / self.rho_w - 1 )
         return w
     
+    ################################################
     # Saturation factors 
+    ################################################
+
     def Omega_A(self, c_ca, c_co, x):
         sp = c_ca*c_co*self.K_C/self.K_A - 1 
         Omega_PA = (max(0.0,sp))**self.m
@@ -181,7 +185,9 @@ class LHeureux:
         Omega_DC = (max(0.0, sa))**self.n
         return Omega_PC - self.nu2*Omega_DC 
     
+    ################################################
     # full reaction terms
+    ################################################
     
     # Aragonite
     def R_AR(self, AR, CA, c_ca, c_co, x):
@@ -208,8 +214,9 @@ class LHeureux:
         return self.Da*( 1 - phi )*\
                ( AR * self.Omega_A(c_ca, c_co, x) - self.lamb * CA * self.Omega_C(c_ca, c_co) )
     
-    
+    ################################################
     # diffusion coefficients, dissolved ions (eqn 6)
+    ################################################
     
     # Ca ions
     def d_c_ca(self, phi):
@@ -221,155 +228,117 @@ class LHeureux:
         # scaled with D_Ca_0
         return self.D_CO/self.D_Ca_0*(1 / ( 1 - 2*np.log(phi) ) )
 
+    #################################################################
     ##### functions which calculate the full RHS of eqns 40-43 ######
     #####   with spatial derivative stencils applied for MOL   ######
+    #################################################################
 
     # Aragonite (eqn 40)
     def RHS_AR(self, AR, CA, c_ca, c_co, phi, x, h):
-        
         dAR_dt = np.zeros(len(x))
-        
         u = self.U(phi)
-        
         for i in range(0,len(x)):
-            
             # x = 0 BC, Dirichlet
             if (i==0):
-                dAR_dt[i] = 0
-            
+                dAR_dt[i]= 0
             # x = Lx, no prescribed BC
             elif (i==len(x)-1):
-                dAR_dt[i] = -u[i]*( AR[i] - AR[i-1] ) / h + self.R_AR(AR[i], CA[i], c_ca[i], c_co[i], x[i])
-            
+                dAR_dt[i]= -u[i]*(AR[i]-AR[i-1])/h + self.R_AR(AR[i],CA[i],c_ca[i],c_co[i],x[i])
             else:
-                dAR_dt[i] = -u[i]*( AR[i+1] - AR[i-1] ) / (2*h) + self.R_AR(AR[i], CA[i], c_ca[i], c_co[i], x[i])    
-        
+                dAR_dt[i]= -u[i]*(AR[i+1]-AR[i-1])/(2*h) + self.R_AR(AR[i],CA[i],c_ca[i],c_co[i], x[i])    
         return dAR_dt
 
     # Calcite (eqn 41)
     def RHS_CA(self, AR, CA, c_ca, c_co, phi, x, h):
-        
         dCA_dt = np.zeros(len(x))
-        
         u = self.U(phi)
-        
         for i in range(0,len(x)):
-            
             # x = 0 BC, Dirichlet
             if (i==0):
-                dCA_dt[i] = 0
-            
+                dCA_dt[i]= 0
             # x = Lx, no prescribed BC
             elif (i==len(x)-1):
-                dCA_dt[i] = -u[i]*( CA[i] - CA[i-1] ) / h + self.R_CA(AR[i], CA[i], c_ca[i], c_co[i], x[i])
-                
+                dCA_dt[i]= -u[i]*( CA[i]-CA[i-1] ) / h + self.R_CA(AR[i], CA[i], c_ca[i], c_co[i], x[i])
             else:
-                dCA_dt[i] = -u[i]*( CA[i+1] - CA[i-1] ) / (2*h) + self.R_CA(AR[i], CA[i], c_ca[i], c_co[i], x[i])     
-        
+                dCA_dt[i]= -u[i]*( CA[i+1]-CA[i-1])/(2*h)+self.R_CA(AR[i],CA[i],c_ca[i],c_co[i],x[i])
         return dCA_dt
     
     # Ca ions (eqn 42)
     def RHS_c_ca(self, AR, CA, c_ca, c_co, phi, x, h):
-        
         dc_ca_dt = np.zeros(len(x))
-        
         w = self.W(phi)
-        
         phi_half = np.zeros(len(x)-1)
         d_ca_half = np.zeros(len(x)-1)
-        
         for i in range(0,len(x)-1):
-            
             phi_half[i] = ( phi[i+1] + phi[i] ) / 2
             d_ca_half[i] = ( self.d_c_ca(phi[i+1]) + self.d_c_ca(phi[i]) ) / 2
-        
         for i in range(0,len(x)):
-            
             # x = 0 BC, Dirichlet
             if (i==0):
                 dc_ca_dt[i] = 0
-            
             # x = Lx BC, df/dx = 0
             elif (i==len(x)-1):
-                
                 dc_ca_dt[i] = - w[i]*( c_ca[i] - c_ca[i-1] ) / (h) +\
                               ( 1 / phi[i] ) * ( phi[i-2] * self.d_c_ca(phi[i-2]) * c_ca[i-2] -\
                                                    phi[i-1] * self.d_c_ca(phi[i-1]) * c_ca[i-1] ) / h**2  +\
                               self.R_c_ca(AR[i], CA[i], c_ca[i], c_co[i], phi[i], x[i]) 
-                
             else:
                 dc_ca_dt[i] = - w[i]*( c_ca[i+1] - c_ca[i-1] ) / (2*h) +\
-                              ( 1 / phi[i] ) * ( phi_half[i]   * d_ca_half[i]   * ( c_ca[i+1] - c_ca[i] ) -\
-                                                 phi_half[i-1] * d_ca_half[i-1] * ( c_ca[i] - c_ca[i-1] ) ) / h**2 +\
+                              ( 1 / phi[i] ) * ( phi_half[i]   * d_ca_half[i]  *(c_ca[i+1] - c_ca[i]) -\
+                                                 phi_half[i-1] * d_ca_half[i-1]*(c_ca[i] - c_ca[i-1]) ) / h**2 +\
                               self.R_c_ca(AR[i], CA[i], c_ca[i], c_co[i], phi[i], x[i])
-                
         return dc_ca_dt
     
     # CO3 ions (eqn 42)
     def RHS_c_co(self, AR, CA, c_ca, c_co, phi, x, h):
-        
         dc_co_dt = np.zeros(len(x))
-        
         w = self.W(phi)
-        
         phi_half = np.zeros(len(x)-1)
         d_co_half = np.zeros(len(x)-1)
-        
         for i in range(0,len(x)-1):
-            
             phi_half[i] = ( phi[i+1] + phi[i] ) / 2
             d_co_half[i] = ( self.d_c_co(phi[i+1]) + self.d_c_co(phi[i]) ) / 2
-        
         for i in range(0,len(x)):
-            
             # x = 0 BC, Dirichlet
             if (i==0):
                 dc_co_dt[i] = 0
-            
             # x = Lx BC, df/dx = 0
             elif (i==len(x)-1):
                 dc_co_dt[i] = - w[i]*( c_co[i] - c_co[i-1] ) / h +\
                               ( 1 / phi[i] )*( phi[i-2] * self.d_c_co(phi[i-2]) * c_co[i-2] -\
                                                phi[i-1] * self.d_c_co(phi[i-1]) * c_co[i-1] ) / h**2 +\
                               self.R_c_co(AR[i], CA[i], c_ca[i], c_co[i], phi[i], x[i]) 
-                
             else:
                 dc_co_dt[i] = - w[i]*( c_co[i+1] - c_co[i-1] ) / (2*h) +\
                               (1 / phi[i] )*( phi_half[i]   * d_co_half[i]   * ( c_co[i+1] - c_co[i] ) -\
                                               phi_half[i-1] * d_co_half[i-1] * ( c_co[i] - c_co[i-1] ) ) / h**2 +\
                               self.R_c_co(AR[i], CA[i], c_ca[i], c_co[i], phi[i], x[i])
-                     
         return dc_co_dt
 
     # porosity (eqn 43)
     def RHS_phi(self, AR, CA, c_ca, c_co, phi, x, h):
-        
         dphi_dt = np.zeros(len(x))
-        
         w = self.W(phi)
-        
-        
         for i in range(0,len(x)):
-            
             # x = 0 BC, Dirichlet
             if (i==0):
                 dphi_dt[i] = 0
-            
             # x = Lx BC, df/dx = 0
             elif (i==len(x)-1):
                 dphi_dt[i] = - ( w[i] * phi[i] - w[i-1] * phi[i-1] ) / h +\
                              self.d_phi*( phi[i-2] - phi[i-1] ) / h**2 +\
                              self.R_phi(AR[i], CA[i], c_ca[i], c_co[i], phi[i], x[i])
-                
             else:
                 dphi_dt[i] = -( w[i+1] * phi[i+1] - w[i-1] * phi[i-1] ) / (2*h) +\
                             self.d_phi * ( phi[i+1] - 2*phi[i] + phi[i-1] ) / h**2 +\
                             self.R_phi(AR[i], CA[i], c_ca[i], c_co[i], phi[i], x[i])
-                
         return dphi_dt
-    
+   
+    ########################################## 
     # combined call for the RHS functions, 
     # for use in the scipy ivp_solve function
+    ########################################## 
+
     def X_RHS(self, t, X, nnx, x, h):
         
         dAR_dt   = self.RHS_AR(  X[0:nnx], X[nnx:2*nnx], X[2*nnx:3*nnx], X[3*nnx:4*nnx], X[4*nnx:5*nnx], x, h)
@@ -380,14 +349,13 @@ class LHeureux:
         
         return np.concatenate((dAR_dt, dCA_dt, dc_ca_dt, dc_co_dt, dphi_dt)) 
 
-
-#################################################################
+###############################################################################
 
 # create instance of the model class
 lh = LHeureux()
 
 # set up the spatial grid
-nnx = 200
+nnx = 500
 L_x = 500/lh.x_scale 
 h = L_x/(nnx-1)
 x = np.linspace(0, L_x,nnx)
@@ -408,25 +376,33 @@ X_new = np.zeros([nnx*5])
 
 # time integration values
 t0 = 0.0
-tf = 50/lh.t_scale # sim time in a, scaled to dimensionless form 
+tf = 250/lh.t_scale # sim time in a, scaled to dimensionless form 
 
 # set the timestep manually ONLY used in Euler mode
-delta_t = 1.319e-2*1/lh.t_scale   # timestep in a, 1.13e-2/tsc = 10^-6 in scaled time
+delta_t = 0.001/lh.t_scale #1.319e-2*1/lh.t_scale   # timestep in a, 1.13e-2/tsc = 10^-6 in scaled time
 t_arr = np.arange(t0, tf, delta_t)
-
 
 # labels corresponding the the soln variables, for print statements
 labels=['AR  ', 'CA  ', 'c_ca', 'c_co', 'phi ']
 
 # run settings 
 verbose = True        # print out extra info at each step, for debugging
-method = 'RK23'      # choice of method for time integration
+method = 'Euler'      # choice of method for time integration
                       # options currently: 'Euler','RK23','RK45','DOP853'
                       
 print_freq = 100      # frequency of print statements in Euler mode
 output_freq = 10      # frequency of soln storage
 
 t_eval = t_arr[::output_freq] # times at which to store the solution, for ivp routines
+
+velstats_file=open('stats_vel.ascii',"w")
+stats_file=open('stats_fields.ascii',"w")
+
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
 
 if (method=='Euler'):
     
@@ -439,8 +415,8 @@ if (method=='Euler'):
     for i in range(0,len(t_arr)):
         
         if (i%print_freq==0):
-            print('***********************istep=',i)
-            print('t=%.2e / tf=%.2e' %(t_arr[i],tf))
+            print('*****istep=',i,'***********************')
+            print('t=%.2e / tf=%.2e' %(t_arr[i]*lh.t_scale,tf*lh.t_scale))
         
         if (i%output_freq==0):
             # first append current soln to storage
@@ -468,18 +444,37 @@ if (method=='Euler'):
                 X_new[j*nnx:(j+1)*nnx] = np.clip(X_new[j*nnx:(j+1)*nnx], 0.01, 1.0)
             else:
                 X_new[j*nnx:(j+1)*nnx] = np.clip(X_new[j*nnx:(j+1)*nnx], 0.0, 1.0e5)
+
+        velstats_file.write("%d %6e %6e %6e %6e \n" % (i,np.min(lh.U(X_new[4*nnx:5*nnx])),\
+                                                         np.max(lh.U(X_new[4*nnx:5*nnx])),\
+                                                         np.min(lh.W(X_new[4*nnx:5*nnx])),\
+                                                         np.max(lh.W(X_new[4*nnx:5*nnx]))))
+        velstats_file.flush()
+
+        stats_file.write("%d %e %e %e %e %e %e %e %e %e %e\n" %\
+                                                      (i,np.min(X_new[0*nnx:1*nnx]),\
+                                                         np.max(X_new[0*nnx:1*nnx]),\
+                                                         np.min(X_new[1*nnx:2*nnx]),\
+                                                         np.min(X_new[1*nnx:2*nnx]),\
+                                                         np.min(X_new[2*nnx:3*nnx]),\
+                                                         np.min(X_new[2*nnx:3*nnx]),\
+                                                         np.min(X_new[3*nnx:4*nnx]),\
+                                                         np.min(X_new[3*nnx:4*nnx]),\
+                                                         np.min(X_new[4*nnx:5*nnx]),\
+                                                         np.max(X_new[4*nnx:5*nnx])))
+
             
         # print out min, max values for each t step if needed
-        if(verbose):
-            if (i%print_freq==0):
-                for j in range(0,5):
-                    # maximum and min soln values
-                    print('%s min, max = %.3f , %.3f' %(labels[j], np.min(X_new[j*nnx:(j+1)*nnx]), np.max(X_new[j*nnx:(j+1)*nnx])))
-                
-                print('U    min, max = %.3f , %.3f' %(np.min(lh.U(X_new[4*nnx:5*nnx])), np.max(lh.U(X_new[4*nnx:5*nnx]))))
-                print('W    min, max = %.3f , %.3f' %(np.min(lh.W(X_new[4*nnx:5*nnx])), np.max(lh.W(X_new[4*nnx:5*nnx]))))
+        if verbose and i%print_freq==0:
+           for j in range(0,5):
+               # maximum and min soln values
+               print('%s (m,M) = %.3f , %.3f' %(labels[j],np.min(X_new[j*nnx:(j+1)*nnx]),\
+                                                          np.max(X_new[j*nnx:(j+1)*nnx])))
+           print('U    (m,M) = %.3f , %.3f' %(np.min(lh.U(X_new[4*nnx:5*nnx])),\
+                                              np.max(lh.U(X_new[4*nnx:5*nnx]))))
+           print('W    (m,M) = %.3f , %.3f' %(np.min(lh.W(X_new[4*nnx:5*nnx])),\
+                                              np.max(lh.W(X_new[4*nnx:5*nnx]))))
             
-        
         if (i%output_freq==0):
             vel_U.append(lh.U(X_new[4*nnx:5*nnx]))            
             vel_W.append(lh.W(X_new[4*nnx:5*nnx]))            
@@ -493,8 +488,6 @@ if (method=='Euler'):
         # move the new values into X
         X = np.copy(X_new)
 
-    end = time.time()    
-
     # convert soln lists to arrays
     soln = np.array(soln)
     vel_U = np.array(vel_U)
@@ -505,10 +498,9 @@ if (method=='Euler'):
     vel_U = np.transpose(vel_U)
     vel_W = np.transpose(vel_W)
     
-    
-elif(method=='RK23' or method=='RK45' or method=='DOP853'):
-    # use the scipy solve_ivp
+elif(method=='RK23' or method=='RK45' or method=='DOP853'): # use the scipy solve_ivp
 
+    print('****************************')
     print('using ivp with method=%s'%(method))
     start = time.time()
     if (output_freq==1):
@@ -517,7 +509,6 @@ elif(method=='RK23' or method=='RK45' or method=='DOP853'):
     else:
         # use subsampled t_arr points for evaluation
         soln_full = solve_ivp(lh.X_RHS, (t0,tf), X, args=(nnx, x, h), method=method, t_eval=t_eval)
-    end = time.time()
     
     # produce separate soln and t_arrs to match output of Euler mode
     soln = soln_full.y
@@ -526,7 +517,6 @@ elif(method=='RK23' or method=='RK45' or method=='DOP853'):
 
     print('nb of time steps=',len(t_arr))
     print('average time step=', tf/len(t_arr))
-    
 
     delta_t=1
     vel_U=np.array(lh.U(soln[4*nnx:,:]))
@@ -535,8 +525,9 @@ else:
     print('selected method is not a supported choice')
     print('Options are Euler, RK23, RK45, DOP853')
 
+print('***********************istep=',i)
+print('time elapsed=',  time.time()-start)
 
-print('time elapsed=',  end-start)
 ###########################################################################################
 
 ##### save results to vtu, ascii format
